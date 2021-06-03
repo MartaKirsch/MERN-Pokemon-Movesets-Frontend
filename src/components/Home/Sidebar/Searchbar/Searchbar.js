0a3b6eea856ac2f5523f6d858fa5ad53770c3a01
Marta Kirsch <martakirsch00@gmail.com>
2021-06-03 11:18:43 +0200

    refactored searchbar's loading hints with hooks

--- src/components/Home/Sidebar/Searchbar/Searchbar.js
+++ src/components/Home/Sidebar/Searchbar/Searchbar.js
@@ -2,90 +2,19 @@ import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Wrapper from 'components/Home/Sidebar/Searchbar/Wrapper';
 import HintsList from 'components/Home/Sidebar/Searchbar/HintsList/HintsList';
+import useHideHints from 'hooks/useHideHints';
+import useLoadHints from 'hooks/useLoadHints';
 
 const Searchbar = () => {
 
-  const [isVisible, setIsVisible] = useState(false);
-  const [pokedex, setPokedex] = useState([]);
-  const [hints, setHints] = useState([]);
-  const [isPending, setIsPending] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const ref = useRef(null);
   const [searchPhrase, setSearchPhrase] = useState("");
 
-  const ref = useRef(null);
-
-  useEffect(()=>{
-    //onclick event on window
-    window.addEventListener('click',checkClicked);
-
-    let source = axios.CancelToken.source();
-
-    setIsPending(true);
-    setIsError(false);
-
-    //load full pokedex list for hints
-    axios.get(`/pokemon/loadFullPokedex`, {cancelToken: source.token}).then(res=>{
-      if(res.statusText!=="OK")
-      {
-        throw new Error("Error");
-      }
-      setPokedex(res.data.pokedex);
-      setIsPending(false);
-
-    }).catch(err=>{
-      if(err.name && err.name !== "AbortError")
-      {
-        setIsPending(false);
-        setIsError(true);
-      }
-    });
-
-
-    return () => {
-      source.cancel("Cancelling in cleanup");
-      window.removeEventListener('click',checkClicked);
-    }
-  },[]);
-
-  const checkClicked = e => {
-    if(!ref.current)
-      return;
-
-    if(e.target !== ref.current.querySelector('input') && e.target.parentNode !== ref.current)
-    {
-      setIsVisible(false);
-    }
-  }
+  const { isVisible, setIsVisible } = useHideHints(ref);
+  const { selectedHints, hints, isError, isPending } = useLoadHints(`/pokemon/loadFullPokedex`,searchPhrase);
 
   const handleFocus = () => {
     setIsVisible(true);
-  };
-
-  const handleChange = (val) => {
-    setSearchPhrase(val);
-
-    //check if the field is empty
-    if(val==="")
-    {
-      setHints([]);
-      return;
-    }
-
-    const reg = new RegExp('^'+val);
-
-    //reset hints
-    setHints([]);
-
-    //load new hints
-    let arr=[];
-
-    pokedex.forEach(({name}) => {
-      if(name.match(reg))
-        arr.push(name);
-    });
-
-    //set new hints
-    setHints(arr);
   };
 
   return(
@@ -95,7 +24,7 @@ const Searchbar = () => {
         type="search"
         placeholder="Search for Pokemon"
         onFocus={handleFocus}
-        onChange={e=>handleChange(e.target.value)}
+        onChange={e=>setSearchPhrase(e.target.value)}
         value={searchPhrase}
         />
         <span className="line"></span>
@@ -103,7 +32,7 @@ const Searchbar = () => {
         display={isVisible.toString()}
         isError={isError}
         isPending={isPending}
-        hints={hints}
+        hints={selectedHints}
         />
       </span>
     </Wrapper>
