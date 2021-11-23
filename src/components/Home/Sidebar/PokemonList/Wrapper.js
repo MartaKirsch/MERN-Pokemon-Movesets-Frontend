74f33fdf7a39ac8cbf18541fd890355fba1cc7c6
Marta Kirsch <martakirsch00@gmail.com>
2021-11-23 09:31:48 +0100

    updated package json

--- src/components/Home/Sidebar/PokemonList/Wrapper.js
+++ src/components/Home/Sidebar/PokemonList/Wrapper.js
@@ -1,26 +1,24 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: auto;
 
-  width:100%;
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  overflow-y:auto;
-
-  ul
-  {
-    width:100%;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
+  ul {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
-  scrollbar-color: #7D8D9C transparent;
+  scrollbar-color: #7d8d9c transparent;
   scrollbar-width: thin;
 
   &::-webkit-scrollbar {
-   width: 0.5vw;
+    width: 0.5vw;
   }
 
   /* &::-webkit-scrollbar-track {
@@ -28,26 +26,23 @@ const Wrapper = styled.div`
   } */
 
   &::-webkit-scrollbar-thumb {
-   background: #7D8D9C;
+    background: #7d8d9c;
   }
 
   &::-webkit-scrollbar-thumb:hover {
-   background: #6c7c8b;
+    background: #6c7c8b;
   }
 
-  h2
-  {
-    color:white;
+  h2 {
+    color: white;
   }
 
-  @media(min-width: 200px)
-  {
-    height:90%;
+  @media (min-width: 200px) {
+    height: 90%;
   }
 
-  @media(min-width: 576px)
-  {
-    height:85%;
+  @media (min-width: 576px) {
+    height: 85%;
   }
 `;
 
